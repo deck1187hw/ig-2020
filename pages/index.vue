67b850895a70772c48426bc66318074474ad96a5
Miguel Puig <Miguel.PuigGarcia@ig.com>
2019-11-05 13:37:07 +0000

    dummy change

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
   <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">IG on JAMstack</h1>
+      <h1 class="display-4 font-italic">IG</h1>
       <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
       <p class="lead mb-0">
       
